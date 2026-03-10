2e40fbdd461d6ca32da4aab09e1fed57c8b8137d
Asgeir Albretsen <asgeir.albretsen@gmail.com>
2026-03-10 11:16:17 +0000

    Allow dev.albretsen.no in Vite dev server

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3001,
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: 3000,
+    allowedHosts: ['dev.albretsen.no'],
   },
 })
